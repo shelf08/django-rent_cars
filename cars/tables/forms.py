9618be4522249582d278ba85cf779870e63c6cfe
shelf08 <vip.lobtsov@gmail.com>
2024-12-20 18:17:00 +0300

    i think web app is done for finish

--- cars/tables/forms.py
+++ cars/tables/forms.py
@@ -1,4 +1,12 @@
 from django import forms
+from django.core.exceptions import ValidationError
+import re
 
 class LoginForm(forms.Form):
     phone = forms.CharField(max_length=11, label='Номер телефона')
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if not re.match(r'^89\d{9}$', phone):
+            raise ValidationError('Введите номер телефона в формате 8-9...')
+        return phone
